94c3f6cfdb3fe63aa739b58095315ead7f4e13bf
Prof Lucas <Seu Email>
2024-08-29 21:35:46 -0300

    Aula 04: Estruturação de Entidades

--- CantinaSenac/Curso.cs
+++ CantinaSenac/Curso.cs
@@ -4,7 +4,5 @@ public class Curso
     public string Nome { get; set; }
     public DateTime DataInicio { get; set; }
     public DateTime DataFinal { get; set; }
-
-    //horario
     public string Descricao { get; set; }
 }
