63cb332931106730416958cc8428d969dc233edc
prof-lucas-senac <lucas.barros@prof.sc.senac.br>
2024-09-16 20:58:29 -0300

    Delete CantinaSenac/Postagem.cs

--- CantinaSenac/Postagem.cs
+++
@@ -1,7 +0,0 @@
-public abstract class Postagem {
-    public int Id { get; set; }
-    public string Descricao { get; set; }
-    public DateTime DataPublicacao { get; set; }
-    public Usuario Usuario { get; set; }
-    public List<Comentario> Comentarios { get; set; }    
-}
