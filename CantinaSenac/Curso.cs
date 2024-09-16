5a579694892d6be6f8db4df6985ac1be41e72be5
prof-lucas-senac <lucas.barros@prof.sc.senac.br>
2024-09-16 20:58:04 -0300

    Delete CantinaSenac/Curso.cs

--- CantinaSenac/Curso.cs
+++
@@ -1,8 +0,0 @@
-public class Curso
-{
-    public int Id { get; set; }
-    public string Nome { get; set; }
-    public DateTime DataInicio { get; set; }
-    public DateTime DataFinal { get; set; }
-    public string Descricao { get; set; }
-}
