597b765d10a5f340392fc9c542f958e04afea6fc
ArthurRF632 <iajava159@gmail.com>
2024-09-16 22:25:19 -0300

    Tela Feedback

--- CantinaSenac/obj/Debug/net8.0/CantinaSenac.AssemblyInfo.cs
+++ CantinaSenac/obj/Debug/net8.0/CantinaSenac.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CantinaSenac")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9a3b9420e5fe6db3f599371875478e9274ca31f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15c4ad95b1faa6b65fe6095b35228a72266f9220")]
 [assembly: System.Reflection.AssemblyProductAttribute("CantinaSenac")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CantinaSenac")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
